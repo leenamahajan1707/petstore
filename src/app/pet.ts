f25d8439d81ea9edad1999bbf678b7e3a6d520c8
leenamahajan1707 <65151780+leenamahajan1707@users.noreply.github.com>
2020-09-04 21:30:28 +0530

    ts file included manually

---
+++ src/app/pet.ts
@@ -0,0 +1,6 @@
+export interface Pet {
+    id: number;
+    price: number;
+    name: string;
+    img: string; 
+  }
---
+++ src/app/petlist.ts
@@ -0,0 +1,8 @@
+import { Pet } from "./pet";
+
+export const PETS: Pet[] = [
+    { id: 1, price: 500, img: "assets/product/1.jpg", name: 'Salmon Oil' },
+    { id: 2, price: 550, img: "assets/product/2.jpg", name: 'karma pet food' },
+    { id: 3, price: 600, img: "assets/product/3.jpg", name: 'simple holding belt' },
+
+];
